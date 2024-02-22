9a061bba4ec1f2e6c5a62d4a0ec3071c1d0d4f52
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-02-22 16:47:40 +0200

    (lint): remove unused linters

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -2,26 +2,10 @@ return {
 	"mfussenegger/nvim-lint",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local lint = require("lint")
-
-		local linters_by_ft = {
-			python = { "flake8" },
+		require("lint").linters_by_ft = {
 			sh = { "shellcheck" },
 			zsh = { "shellcheck" },
-			c = { "betty_style", "betty_doc" },
-			cpp = { "betty_style", "betty_doc" },
 		}
-
-		local linters = {
-			betty_style = require("linters.betty_style"),
-			betty_doc = require("linters.betty_doc"),
-		}
-
-		lint.linters_by_ft = linters_by_ft
-
-		for linter, opts in pairs(linters) do
-			lint.linters[linter] = opts
-		end
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged", "TextChangedI" }, {
 			group = vim.api.nvim_create_augroup("lint", { clear = true }),

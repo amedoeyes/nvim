5a63048b2ea6cd324f14e81b3d739bda9f3c15ed
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-05-07 20:29:01 +0300

    (core/commands): refactor create script command

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -2,15 +2,12 @@ local util = require("core.util")
 local command = vim.api.nvim_create_user_command
 local fn = vim.fn
 
-local function create_script(path, shabang)
+local function create_script(path, env)
 	local file = io.open(path, "w")
-
 	if file then
-		file:write(shabang .. "\n\n")
+		file:write("#!" .. env .. "\n\n")
 		file:close()
-
 		fn.system("chmod +x " .. path)
-
 		vim.cmd("e " .. path)
 		vim.cmd("2")
 	else
@@ -18,15 +15,16 @@ local function create_script(path, shabang)
 	end
 end
 
-command("CreateBash", function()
-	local path = fn.input("Path: ", fn.getcwd() .. "/", "file")
-	create_script(path, "#!/usr/bin/env bash")
-end, {})
+local function create_script_command(name, env)
+	command(name, function(opts)
+		local path = #opts.args > 0 and opts.args or fn.input("Path: ", fn.getcwd() .. "/", "file")
+		create_script(path, env)
+	end, { nargs = "?" })
+end
 
-command("CreatePython", function()
-	local path = fn.input("Path: ", fn.getcwd() .. "/", "file")
-	create_script(path, "#!/usr/bin/python3")
-end, {})
+create_script_command("CreateBash", "/usr/bin/env bash")
+create_script_command("CreatePython", "/usr/bin/python3")
+create_script_command("CreateJavaScript", "/usr/bin/node")
 
 command("ProjectRoot", function()
 	vim.cmd("cd " .. util.get_root_dir())

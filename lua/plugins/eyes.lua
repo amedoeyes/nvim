a575c0a44aa3bc96745dd228105028c2ac72eafd
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-12-15 01:44:54 +0200

    config(eyes): disable unused highlights

--- lua/plugins/eyes.lua
+++ lua/plugins/eyes.lua
@@ -1,7 +1,25 @@
 return {
 	"amedoeyes/eyes.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
 	lazy = false,
 	priority = 1000,
-	opts = {},
+	opts = {
+		features = {
+			plugins = {
+				cmp = false,
+				codeium = false,
+				flash = false,
+				helpview = false,
+				illuminate = false,
+				leap = false,
+				markview = false,
+				mini_indentscope = false,
+				neo_tree = false,
+				noice = false,
+				notify = false,
+				telescope = false,
+				undo_tree = false,
+				web_devicons = false,
+			},
+		},
+	},
 }

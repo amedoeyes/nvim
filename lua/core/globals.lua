e529f0ec991cb58e089f8a50ccb23009ae31faac
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-05 02:34:45 +0200

    config(core.globals): config termdebug and markdown

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.g.markdown_recommended_style = 0
+vim.g.termdebug_config = { sign = "" }
+
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0

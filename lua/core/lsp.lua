18fe699d43378dbb6c823ff1d1e5de51f4e167b3
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-10 00:57:34 +0200

    feat(lsp): attach autocmd

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -8,6 +8,25 @@ vim.lsp.util.open_floating_preview = function(contents, syntax, opts)
 	return original_open_floating_preview(contents, syntax, opts)
 end
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(e)
+		local client = vim.lsp.get_client_by_id(e.data.client_id)
+		if not client then return end
+		if client:supports_method(vim.lsp.protocol.Methods.textDocument_codeLens) then
+			vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+				group = vim.api.nvim_create_augroup("eyes.lsp.codelens", { clear = false }),
+				buffer = e.buf,
+				callback = function()
+					if vim.g.codelens then vim.lsp.codelens.refresh({ bufnr = e.buf }) end
+				end,
+			})
+		end
+		if client:supports_method(vim.lsp.protocol.Methods.textDocument_codeLens) then
+			vim.opt_local.foldexpr = "v:lua.vim.lsp.foldexpr()"
+		end
+	end,
+})
+
 vim.lsp.config("*", { root_markers = { ".git" } })
 
 vim.lsp.enable({

908fa7073f45ee1cfc5e8c075f486f964c40b4c7
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-09 06:06:05 +0200

    refactor(ft): remove redundant shader filetypes

--- lua/core/filetypes.lua
+++ lua/core/filetypes.lua
@@ -1,12 +1,4 @@
 vim.filetype.add({
-	extension = {
-		vert = "glsl",
-		geom = "glsl",
-		frag = "glsl",
-		tesc = "glsl",
-		tese = "glsl",
-		comp = "glsl",
-	},
 	pattern = {
 		[".*/sway/.*%.conf"] = "swayconfig",
 	},

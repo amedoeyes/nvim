ddcba8b1616fc50772c802739474851b5029cefd
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-09 21:39:59 +0200

    feat(format): use `vim.lsp.util.apply_text_edits` for formatting

--- lua/core/format.lua
+++ lua/core/format.lua
@@ -1,20 +1,37 @@
+---@param buf integer
+---@param range? {start_line: integer, end_line: integer}
 local function format(buf, range)
 	if vim.bo.formatprg ~= "" then
-		local output = vim.fn.system(vim.bo.formatprg, vim.api.nvim_buf_get_lines(buf, 0, -1, true))
-		if vim.v.shell_error ~= 0 then return 1 end
-		local formatted_lines = vim.split(output, "\n", { plain = true, trimempty = true })
-		if range then
-			formatted_lines = vim.list_slice(formatted_lines, range.start[1], range["end"][1])
-			vim.api.nvim_buf_set_lines(buf, range.start[1] - 1, range["end"][1], false, formatted_lines)
-		else
-			vim.api.nvim_buf_set_lines(buf, 0, -1, false, formatted_lines)
+		local start_line = range and (range.start_line - 1) or 0
+		local end_line = range and range.end_line or -1
+		local original_lines = vim.api.nvim_buf_get_lines(buf, start_line, end_line, true)
+		local formatted_lines = vim.fn.systemlist(vim.bo.formatprg, original_lines)
+		if vim.v.shell_error ~= 0 then return end
+		local diff = vim.diff(
+			table.concat(original_lines, "\n"),
+			table.concat(formatted_lines, "\n"),
+			{ result_type = "indices" }
+		)
+		if vim.tbl_isempty(diff) then return end
+		local text_edits = {}
+		for _, hunk in ipairs(diff) do
+			local start_a, count_a, start_b, count_b = table.unpack(hunk)
+			local replacement_lines = vim.list_slice(formatted_lines, start_b, start_b + count_b - 1)
+			table.insert(text_edits, {
+				range = {
+					start = { line = start_line + start_a - 1, character = 0 },
+					["end"] = { line = start_line + start_a - 1 + count_a, character = 0 },
+				},
+				newText = table.concat(replacement_lines, "\n") .. "\n",
+			})
 		end
+		vim.lsp.util.apply_text_edits(text_edits, buf, "utf-16")
 	else
-		local clients = vim.lsp.get_clients({
-			bufnr = 0,
-			method = vim.lsp.protocol.Methods.textDocument_formatting,
-		})
-		if #clients > 0 then vim.lsp.buf.format({ bufnr = buf, range = range }) end
+		if
+			not vim.tbl_isempty(vim.lsp.get_clients({ bufnr = buf, method = "textDocument/formatting" }))
+		then
+			vim.lsp.buf.format({ bufnr = buf, range = range })
+		end
 	end
 	vim.diagnostic.show(nil, 0)
 end
@@ -25,16 +42,10 @@ function FormatExpr()
 		format(buf)
 		return 0
 	end
-	local start_lnum = vim.v.lnum
-	local end_lnum = start_lnum + vim.v.count - 1
-	if start_lnum <= 0 or end_lnum <= 0 then return 0 end
-	local end_line = vim.fn.getline(end_lnum)
-	local end_col = end_line:len()
-	local range = {
-		start = { start_lnum, 0 },
-		["end"] = { end_lnum, end_col },
-	}
-	format(buf, range)
+	local start_line = vim.v.lnum
+	local end_line = start_line + vim.v.count - 1
+	if start_line <= 0 or end_line <= 0 then return 0 end
+	format(buf, { start_line = start_line, end_line = end_line })
 	return 0
 end
 

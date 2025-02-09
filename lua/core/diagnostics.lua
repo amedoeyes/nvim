c81bcca4bbca98f8adada1ea647cde20169c9ca1
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-10 00:58:54 +0200

    feat(diagnostic): disable virtual text

--- lua/core/diagnostics.lua
+++ lua/core/diagnostics.lua
@@ -1,16 +1,12 @@
-local icons = {
-	[vim.diagnostic.severity.ERROR] = " ",
-	[vim.diagnostic.severity.WARN] = " ",
-	[vim.diagnostic.severity.INFO] = " ",
-	[vim.diagnostic.severity.HINT] = " ",
-}
-
 vim.diagnostic.config({
-	virtual_text = {
-		source = false,
-		prefix = function(d) return icons[d.severity] end,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+		},
 	},
-	signs = { text = icons },
 	float = {
 		header = "",
 		border = "single",

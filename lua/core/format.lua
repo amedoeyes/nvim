b5ccd85b9e5d6dee441ead97da5d5eacff43bdc9
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-10 04:25:51 +0200

    fix(format): edge cases (this time for sure...)

--- lua/core/format.lua
+++ lua/core/format.lua
@@ -8,32 +8,44 @@ local function format(buf, range)
 		local formatted_lines = vim.fn.systemlist(vim.bo.formatprg, original_lines)
 		if vim.v.shell_error ~= 0 then return end
 		local diff = vim.diff(
-			table.concat(original_lines, "\n"),
-			table.concat(formatted_lines, "\n"),
+			table.concat(original_lines, "\n") .. "\n",
+			table.concat(formatted_lines, "\n") .. "\n",
 			{ result_type = "indices" }
 		)
 		if vim.tbl_isempty(diff) then return end
 		local text_edits = {}
 		for _, hunk in ipairs(diff) do
-			local start_a, count_a, start_b, count_b = unpack(hunk)
-			local replacement_lines = vim.list_slice(formatted_lines, start_b, start_b + count_b - 1)
+			local start_og, count_og, start_fmt, count_fmt = unpack(hunk)
+			local end_og = start_og + count_og - 1
+			local end_fmt = start_fmt + count_fmt - 1
+			local start_og_adj = start_og + (count_og == 0 and 1 or 0)
+			local end_og_adj = end_og + ((count_og == 0 and 2 or 0) + (count_fmt == 0 and 1 or 0))
+			local end_fmt_adj = end_fmt + (count_og == 0 and 1 or 0)
+			local replacement_lines = vim.list_slice(formatted_lines, start_fmt, end_fmt_adj)
 			table.insert(text_edits, {
 				range = {
-					start = { line = start_line + start_a - 1, character = 0 },
-					["end"] = { line = start_line + start_a - 1 + count_a, character = 0 },
+					start = { line = start_line + start_og_adj - 1, character = 0 },
+					["end"] = {
+						line = start_line + end_og_adj - 1,
+						character = (count_fmt == 0) and 0 or #original_lines[end_og_adj],
+					},
 				},
-				newText = table.concat(replacement_lines, "\n") .. "\n",
+				newText = table.concat(replacement_lines, "\n"),
 			})
 		end
 		vim.lsp.util.apply_text_edits(text_edits, buf, "utf-16")
 	else
-		if
-			not vim.tbl_isempty(vim.lsp.get_clients({ bufnr = buf, method = "textDocument/formatting" }))
-		then
-			vim.lsp.buf.format({ bufnr = buf, range = range })
+		local clients = vim.lsp.get_clients({ bufnr = buf, method = "textDocument/formatting" })
+		if not vim.tbl_isempty(clients) then
+			vim.lsp.buf.format({
+				bufnr = buf,
+				range = range and {
+					start = { line = range.start_line, character = 0 },
+					["end"] = { line = range.end_line, character = 0 },
+				} or nil,
+			})
 		end
 	end
-	vim.diagnostic.show(nil, 0)
 end
 
 function FormatExpr()
